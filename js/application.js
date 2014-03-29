35c9cabe70712c39b4af587ea385f378d8acc450
Zach <zkaarvik@gmail.com>
2014-03-29 19:06:03 -0400

    added maps (exclaimation mark)

--- js/application.js
+++ js/application.js
@@ -11,8 +11,17 @@
       }
       $(inputFileList).html('<ul>' + output.join('') + '</ul>');
 
-      i!==1 ? $(fileInputSelection).val(i + ' files selected') : $(fileInputSelection).val(i + ' file selected');
-
+      i !== 1 ? $(fileInputSelection).val(i + ' files selected') : $(fileInputSelection).val(i + ' file selected');
   }
-
   $(fileSelection).change(handleFileSelect);
+
+  var map;
+  function initialize() {
+      var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8
+      };
+      map = new google.maps.Map(document.getElementById("map-canvas"),
+          mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
