c51586ce190d3b1b6f09a7cc1f03721588f3bd7a
Zach <zkaarvik@gmail.com>
2014-03-30 13:45:38 -0400

    replaced exif library, places markers on maps successfully

--- js/application.js
+++ js/application.js
@@ -5,31 +5,42 @@
       var files = evt.target.files; // FileList object
       // files is a FileList of File objects. List some properties.
       var output = [];
+      var fileCount = 0;
       for (var i = 0, f; f = files[i]; i++) {
           if (!f.type.match('image.*')) {
               continue;
           }
+          fileCount++;
 
+          //Check if last file
+          var isLastFile = files[i+1] ? false : true;
+
+          //initialize reader
           var reader = new FileReader();
-
-          reader.onload = (function(theFile) {
+          reader.onloadend = (function(theFile) {
               return function(e) {
-                  //Create an img element with the image
-                  var img = $('<img class="gpsImage">'); 
-                  img.attr('src', e.target.result);
-                  img.attr('exif', true);
-                  img.attr('style', "width: 10%; ");
-                  img.appendTo(fileArea);
+                  //Get all the exif data
+                  var exif = EXIF.readFromBinaryFile(new BinaryFile(this.result));
+                  var gps = {
+                      latitudeRef: exif.GPSLatitudeRef,
+                      latitude: exif.GPSLatitude,
+                      longitudeRef: exif.GPSLongitudeRef,
+                      longitude: exif.GPSLongitude
+                  };
+                  //convert to google maps format coordinates 
+                  gps = convertToGoogleGps(gps);
+                  //If there is GPS info, place marker
+                  if (gps) addMarker(gps);
               };
           })(f);
 
-          reader.readAsDataURL(f);
+          reader.readAsBinaryString(f);
 
           output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ', f.size, ' bytes, last modified: ', f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a', '</li>');
       }
-      $(inputFileList).html('<ul>' + output.join('') + '</ul>');
 
-      i !== 1 ? $(fileInputSelection).val(i + ' files selected') : $(fileInputSelection).val(i + ' file selected');
+      $(inputFileList).html('<ul>' + output.join('') + '</ul>');
+      fileCount === 1 ? $(fileInputSelection).val(fileCount + ' file selected') : $(fileInputSelection).val(fileCount + ' files selected');
   }
   $(fileSelection).change(handleFileSelect);
 
@@ -37,7 +48,7 @@
   function initialize() {
       var mapOptions = {
           center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8
+          zoom: 3
       };
       map = new google.maps.Map($(mapCanvas)[0], mapOptions);
   }
@@ -46,9 +57,9 @@
 
 
 
-  function addMarker() {
+  function addMarker(gps) {
       var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(-25.363882, 131.044922),
+          position: new google.maps.LatLng(gps.latitude, gps.longitude),
           map: map,
           animation: google.maps.Animation.DROP,
           title: "I am a picture!"
@@ -63,3 +74,16 @@
    //         }, i * 200);
    //     }
    // }
+
+  function convertToGoogleGps(gps) {
+      if (!(gps.latitude && gps.longitude)) return undefined; // No GPS info  
+
+      // convert from deg/min/sec to decimal for Google  
+      var fLat = (gps.latitude[0] + gps.latitude[1] / 60 + gps.latitude[2] / 3600) * (gps.latitudeRef == "N" ? 1 : -1);
+      var fLong = (gps.longitude[0] + gps.longitude[1] / 60 + gps.longitude[2] / 3600) * (gps.longitudeRef == "W" ? -1 : 1);
+
+      return {
+          latitude: fLat,
+          longitude: fLong
+      };
+  }
